0d25c6d02618fc81cf1ceda7a7823c6b3cd3a03c
Ardie Derrayal <ardiederrayal06@gmail.com>
2019-05-16 18:55:46 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { CustomerServiceComponent } from './customer-service/customer-service.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'insurance', component: InsuranceComponent },
+  { path: 'customerService', component: CustomerServiceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { CustomerServiceComponent } from './customer-service/customer-service.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InsuranceComponent,
+    CustomerServiceComponent,
+    ContactComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
